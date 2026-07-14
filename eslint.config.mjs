ae9be5f5a3c571021e9725521579106d6c04b6fe
unknown <tengwenhui03@gmail.com>
2026-07-14 21:23:52 +0800

    Add ESLint security configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,56 +1,75 @@
-//import js from "@eslint/js";
-//import globals from "globals";
 import { defineConfig } from "eslint/config";
-import reactPlugin from 'eslint-plugin-react';
-import jestPlugin from 'eslint-plugin-jest';
-import testingLibraryPlugin from 'eslint-plugin-testing-library';
-import babelParser from '@babel/eslint-parser';
+import jestPlugin from "eslint-plugin-jest";
+import testingLibraryPlugin from "eslint-plugin-testing-library";
+import securityPlugin from "eslint-plugin-security";
+import babelParser from "@babel/eslint-parser";
+
 export default defineConfig([
-{
-files: ['**/*.{js,jsx}'],
-languageOptions: {
-parser: babelParser,
-parserOptions: {
-requireConfigFile: false,
-babelOptions: {
-presets: ['@babel/preset-react'],
-},
-},
-},
-plugins: {
-react: reactPlugin,
-},
-rules: {
-...reactPlugin.configs.recommended.rules,
-'react/react-in-jsx-scope': 'off',
-},
-settings: {
-react: {
-version: 'detect', // Automatically picks up from package.json
-},
-},
-},
-{
-files: ['**/*.test.jsx'],
-plugins: {
-jest: jestPlugin,
-'testing-library': testingLibraryPlugin,
-},
-rules: {
-...jestPlugin.configs.recommended.rules,
-...testingLibraryPlugin.configs.react.rules,
-'testing-library/await-async-events': 'off',
-},
-languageOptions: {
-globals: {
-test: true,
-expect: true,
-describe: true,
-beforeEach: true,
-afterEach: true,
-it: true,
-jest: true,
-},
-},
-},
+  {
+    files: ["**/*.{js,jsx}"],
+
+    languageOptions: {
+      parser: babelParser,
+
+      parserOptions: {
+        requireConfigFile: false,
+
+        babelOptions: {
+          presets: ["@babel/preset-react"],
+        },
+
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    plugins: {
+      security: securityPlugin,
+    },
+
+    rules: {
+      "security/detect-eval-with-expression": "error",
+    },
+  },
+
+  {
+    files: ["**/*.test.{js,jsx}"],
+
+    plugins: {
+      jest: jestPlugin,
+      "testing-library": testingLibraryPlugin,
+    },
+
+    rules: {
+      ...jestPlugin.configs.recommended.rules,
+      ...testingLibraryPlugin.configs.react.rules,
+      "testing-library/await-async-events": "off",
+    },
+
+    languageOptions: {
+      globals: {
+        test: "readonly",
+        expect: "readonly",
+        describe: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+        it: "readonly",
+        jest: "readonly",
+      },
+    },
+  },
+
+  {
+    ignores: [
+      "node_modules/**",
+      "reports/**",
+      "dist/**",
+      "build/**",
+      "coverage/**",
+      "odc-data/**",
+      "odc-reports/**",
+      "certbot/conf/**",
+    ],
+  },
 ]);
